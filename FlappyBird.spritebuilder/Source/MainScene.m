6559f4f222245eff0ca30f40feb4cb7d9564d826
Maria Luisa <marialuisa@Marias-MacBook-Pro.local>
2015-01-16 19:18:02 -0800

    Automatically Committed for MakeGamesWithUs

--- FlappyBird.spritebuilder/Source/MainScene.m
+++ FlappyBird.spritebuilder/Source/MainScene.m
@@ -27,7 +27,7 @@
     CGPoint _bushParallaxRatio;
     
     CCNode *_parallaxContainer;
-    CCParallaxNode *_parallaxBackgroud;
+    CCParallaxNode *_parallaxBackground;
     
     CCNode *_ground1;
     CCNode *_ground2;
