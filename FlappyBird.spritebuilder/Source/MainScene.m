36cb2dd1a053bc631667d739a44821524fe2c601
Maria Luisa <marialuisa@Marias-MacBook-Pro.local>
2015-02-20 22:13:27 -0800

    Automatically Committed for MakeGamesWithUs

--- FlappyBird.spritebuilder/Source/MainScene.m
+++ FlappyBird.spritebuilder/Source/MainScene.m
@@ -171,8 +171,8 @@
 - (void)update:(CCTime)delta
 {
     _sinceTouch += delta;
-    
-    character.rotation = clampf(character.rotation, -30.f, 90.f);
+
+    //character.rotation = clampf(character.rotation, -30.f, 90.f);
     
     if (character.physicsBody.allowsRotation) {
         float angularVelocity = clampf(character.physicsBody.angularVelocity, -2.f, 1.f);
